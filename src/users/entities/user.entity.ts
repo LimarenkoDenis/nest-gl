966310decbfc05b8bb592c5e4d35051d3011be7f
LimarenkoDenis <limarenkodenis@gmail.com>
2018-12-03 10:11:05 +0200

    add users crud

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,14 @@
+
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  public id: number;
+
+  @Column({ length: 50 })
+  public username: string;
+
+  @Column({ length: 50 })
+  public email: string;
+}
