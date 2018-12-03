4a78137bd5bd322136695810c55379acd286d6b3
LimarenkoDenis <limarenkodenis@gmail.com>
2018-12-03 10:28:21 +0200

    code format

--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -2,25 +2,30 @@ import { CreateUserDto } from './../dto/create.user.dto';
 import { User } from './../entities/user.entity';
 import { Injectable } from '@nestjs/common';
 import { UpdateUserDto } from '../dto/update.user.dto';
- @Injectable()
+
+@Injectable()
 export class UsersService {
   private _users: User[] = [];
-   public async findAll(query: any): Promise<User[]> {
+
+  public async findAll(query: any): Promise<User[]> {
     return Promise.resolve(this._users);
   }
-   public async create(command: CreateUserDto): Promise<User> {
+
+  public async create(command: CreateUserDto): Promise<User> {
     const newUser: User = { ...command, id: this._users.length };
     this._users.push(newUser);
     return Promise.resolve(newUser);
   }
-   public async findOne(id: number): Promise<User> {
+
+  public async findOne(id: number): Promise<User> {
     const user: User | undefined = this._users.find((item: User) => item.id === id);
     if (!user) {
       return Promise.reject(new Error('Can not find user by id'));
     }
     return Promise.resolve(user);
   }
-   public async updateOne(id: number, command: UpdateUserDto): Promise<User> {
+
+  public async updateOne(id: number, command: UpdateUserDto): Promise<User> {
     const index: number = this._users.findIndex((item: User) => item.id === id);
     if (index === -1) {
       return Promise.reject(new Error('Can not find user by id'));
@@ -29,7 +34,8 @@ export class UsersService {
     this._users.splice(index, 1, updatedUser);
     return Promise.resolve(updatedUser);
   }
-   public async deleteOne(id: number): Promise<string> {
+
+  public async deleteOne(id: number): Promise<string> {
     const index: number = this._users.findIndex((item: User) => item.id === id);
     if (index === -1) {
       return Promise.reject(new Error('Can not find user by id'));
