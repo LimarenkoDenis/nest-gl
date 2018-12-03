a3d4f6234448770d3d341d61b750133bb26088a6
LimarenkoDenis <limarenkodenis@gmail.com>
2018-12-03 10:18:43 +0200

    add swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api/v1');
+
+  const options = new DocumentBuilder()
+    .setBasePath('api/v1')
+    .setTitle('My awsome app example')
+    .setDescription('My awsome app API description')
+    .setVersion('1.0')
+    .addTag('v1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(3000);
 }
--- src/users/controllers/users.controller.ts
+++ src/users/controllers/users.controller.ts
@@ -1,10 +1,12 @@
 import { User } from '../entities/user.entity';
 import { UpdateUserDto } from '../dto/update.user.dto';
-import { Controller, Post, Get, Delete, Param, Body, HttpStatus, Res, Query } from '@nestjs/common';
+import { Controller, Post, Get, Delete, Param, Body, HttpStatus, Res, Query, Put } from '@nestjs/common';
 import { CreateUserDto } from '../dto/create.user.dto';
 import { UsersService } from '../services/users.service';
 import { Response } from 'express';
+import { ApiUseTags, ApiOperation, ApiResponse, ApiImplicitQuery } from '@nestjs/swagger';
 
+@ApiUseTags('users')
 @Controller('users')
 export class UsersController {
   public constructor(
@@ -12,6 +14,13 @@ export class UsersController {
   ) { }
 
   @Get()
+  @ApiOperation({ title: 'Get list of users'})
+  @ApiResponse({ status: HttpStatus.OK, description: 'return list of users' })
+  @ApiResponse({ status: HttpStatus.BAD_REQUEST, description: 'Bad request' })
+  @ApiImplicitQuery({ name: 'order', enum: ['abc', 'cba'] }  )
+  @ApiImplicitQuery({ name: 'page' }  )
+  @ApiImplicitQuery({ name: 'limit' }  )
+  @ApiImplicitQuery({ name: 'sort' }  )
   public async findAll(
     @Res() res: Response,
     @Query() queryParams: { page: number, limit: number, order: string, sort: string },
@@ -25,6 +34,9 @@ export class UsersController {
   }
 
   @Post()
+  @ApiOperation({ title: 'Create new user'})
+  @ApiResponse({ status: HttpStatus.OK, description: 'return new created user' })
+  @ApiResponse({ status: HttpStatus.BAD_REQUEST, description: 'Bad request' })
   public async create(@Res() res: Response, @Body() command: CreateUserDto) {
     try {
       const newUser: User = await this._usersService.create(command);
@@ -35,6 +47,9 @@ export class UsersController {
   }
 
   @Get(':id')
+  @ApiOperation({ title: 'Find user'})
+  @ApiResponse({ status: HttpStatus.OK, description: 'return user' })
+  @ApiResponse({ status: HttpStatus.BAD_REQUEST, description: 'Bad request' })
   public async findOne(@Res() res: Response, @Param('id') id: number) {
     try {
       const user: User = await this._usersService.findOne(id);
@@ -44,7 +59,9 @@ export class UsersController {
     }
   }
 
-  @Get(':id')
+  @Put(':id')
+  @ApiOperation({ title: 'Update user'})
+  @ApiResponse({ status: HttpStatus.OK, description: 'return updated user' })
   public async updateOne(@Res() res: Response, @Param('id') id: number, @Body() command: UpdateUserDto) {
     try {
       const newUser: User = await this._usersService.updateOne(id, command);
@@ -55,6 +72,8 @@ export class UsersController {
   }
 
   @Delete(':id')
+  @ApiOperation({ title: 'Delete user'})
+  @ApiResponse({ status: HttpStatus.OK, description: 'return ok' })
   public async deleteOne(@Res() res: Response, @Param('id') id: number) {
     try {
       await this._usersService.deleteOne(id);
