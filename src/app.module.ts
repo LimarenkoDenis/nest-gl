e4ff00c9f4fcd8a8729bb3cfff17e01d801a2131
LimarenkoDenis <limarenkodenis@gmail.com>
2018-12-03 10:27:23 +0200

    add exception filter

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { LoggerModule } from './logger/logger.module';
+import { AllExceptionsFilter } from './filters/exceptionsFilter.filter';
+import { APP_FILTER } from '@nestjs/core';
 
 @Module({
   imports: [UsersModule, LoggerModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: AllExceptionsFilter,
+    },
+  ],
 })
-export class AppModule {}
+export class AppModule { }
